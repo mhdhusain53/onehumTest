334a75c467ab451424cad74436d7f72e33894745
Mohammed Husain <mhusain@acuitas360.com>
2024-12-13 18:23:55 +0530

    third commit

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
     auth: {
       clientId: 'fe83a4cc-f142-4276-af88-eb422a3f8449', // App ID
       authority: 'https://login.microsoftonline.com/97d58c70-a9f4-4aa8-b28f-f833520dc90c',
-      redirectUri: 'https://onehum-mocha.vercel.app/'
+      redirectUri: 'https://onehum-mocha.vercel.app'
       // redirectUri: 'https://onehum.acuitas360.com/'
     },
     cache: {
