44effef386218d9d719e9f9d3881f9a5b61d3924
Mohammed Husain <mhusain@acuitas360.com>
2025-01-06 14:32:07 +0530

    update links5

--- src/Data.js
+++ src/Data.js
@@ -9,7 +9,7 @@ var Cards_Data=[
         link: "https://apps.powerapps.com/play/e/default-97d58c70-a9f4-4aa8-b28f-f833520dc90c/a/e0d37d77-14cc-432f-bac4-6a50f444b640?tenantId=97d58c70-a9f4-4aa8-b28f-f833520dc90c&hint=71758705-5a76-494b-9556-a2a643f06405&sourcetime=1736143913940&source=portal",
         access:"",
         route:"KAM",
-        lastUpdateDate:"01/03/2025"
+        lastUpdateDate:"12/31/2024"
     },{
         id: 7,
         title:"CRM",
@@ -19,7 +19,7 @@ var Cards_Data=[
         link: "https://apps.powerapps.com/play/e/default-97d58c70-a9f4-4aa8-b28f-f833520dc90c/a/36645d39-10d3-4827-92c7-9f54e765e253?tenantId=97d58c70-a9f4-4aa8-b28f-f833520dc90c&sourcetime=1735825671796&source=portal",
         access:"",
         route:"CRM",
-        lastUpdateDate:"12/11/2024"
+        lastUpdateDate:"1/3/2025"
     }
 ,
     {
@@ -31,7 +31,7 @@ var Cards_Data=[
         link: "https://app.powerbi.com/reportEmbed?reportId=cbfd81b6-dae9-4e93-b8d5-dcb5879a113d&autoAuth=true&ctid=97d58c70-a9f4-4aa8-b28f-f833520dc90c",
         access:"",
         route:"Peak",
-        lastUpdateDate:"12/26/2024"
+        lastUpdateDate:"11/30/2024"
     },
     {
         id: 2,
@@ -42,7 +42,7 @@ var Cards_Data=[
         link: "https://apps.powerapps.com/play/e/default-97d58c70-a9f4-4aa8-b28f-f833520dc90c/a/90c7afe1-3051-4eef-9466-42a28ca43ff2?tenantId=97d58c70-a9f4-4aa8-b28f-f833520dc90c&sourcetime=1736141938598",
         access:"",
         route:"Vital360",
-        lastUpdateDate:"12/26/2024"
+        lastUpdateDate:"12/31/2024"
     },
     {
         id: 3,
@@ -53,7 +53,7 @@ var Cards_Data=[
         link: "https://apps.powerapps.com/play/e/default-97d58c70-a9f4-4aa8-b28f-f833520dc90c/a/ec4b471a-6986-49b8-bb6b-c014f2d3b2d8?tenantId=97d58c70-a9f4-4aa8-b28f-f833520dc90c&sourcetime=1733206283864",
         access:"",
         route:"Power-Pack",
-        lastUpdateDate:"12/15/2024"
+        lastUpdateDate:"9/30/2024"
     },
     {
         id: 4,
@@ -64,7 +64,7 @@ var Cards_Data=[
         link: "https://app.powerbi.com/reportEmbed?reportId=dd3da1c2-15d6-4605-8fe9-a38283b6ae39&autoAuth=true&ctid=97d58c70-a9f4-4aa8-b28f-f833520dc90c",
         access:"",
         route:"Self-Serve",
-        lastUpdateDate:"11/30/2024"
+        lastUpdateDate:"1/3/2025"
     },{
         id: 5,
         title:"Pulse",
@@ -74,7 +74,7 @@ var Cards_Data=[
         link: "https://app.powerbi.com/reportEmbed?reportId=bc0af5b1-3e46-4454-aade-79991dd793f4&autoAuth=true&ctid=97d58c70-a9f4-4aa8-b28f-f833520dc90c",
         access:"",
         route:"Pulse",
-        lastUpdateDate:"01/02/2025"
+        lastUpdateDate:"1/3/2025"
     },
 ]
 
